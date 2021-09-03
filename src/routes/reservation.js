a02a3563f69426371b3ebc011cc2255e75fd25e4
Khori Hutama <khori.qq@gmail.com>
2021-09-04 01:37:18 +0700

    remove update & delete reservation

--- src/routes/reservation.js
+++ src/routes/reservation.js
@@ -7,7 +7,5 @@ const ReservationControllers = require('../controllers/reservations')
 router.get('/', asyncMiddleware(ReservationControllers.getAll));
 router.post('/', asyncMiddleware(ReservationControllers.save));
 router.get('/:id', asyncMiddleware(ReservationControllers.getById));
-router.put('/:id', asyncMiddleware(ReservationControllers.update));
-router.delete('/:id', asyncMiddleware(ReservationControllers.delete));
 
 module.exports = router;
